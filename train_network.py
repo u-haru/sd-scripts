f84469bb720b3e8aaf15783de7d794a33cbbc25e
u-haru <40634644+u-haru@users.noreply.github.com>
2025-11-17 17:51:17 +0900

    Fix: ADDifTのステップ数計算を修正する

--- train_network.py
+++ train_network.py
@@ -403,7 +403,8 @@ class NetworkTrainer:
         if first:
             min_t = 0 if args.min_timestep is None else args.min_timestep
             max_t = noise_scheduler.config.num_train_timesteps if args.max_timestep is None else args.max_timestep
-            self.addift_timesteps = get_addift_timesteps(min_t, max_t, args.addift_timesteps_segments, self.current_step//2, data_latents.shape[0]).to(data_latents.device)
+            current_step = self.current_step if args.addift_no_flip else self.current_step // 2
+            self.addift_timesteps = get_addift_timesteps(min_t, max_t, args.addift_timesteps_segments, current_step, data_latents.shape[0]).to(data_latents.device)
         else:
             data_latents, target_latents = target_latents, data_latents
             data_batch, target_batch = target_batch, data_batch
